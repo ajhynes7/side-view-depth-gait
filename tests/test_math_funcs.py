eed5078644bbdec77eaf1c398b66e1d619b77822
Andrew Hynes <andrewjhynes@gmail.com>
2018-07-12 16:25:25 -0400

    Update tests so they all pass.

--- tests/test_math_funcs.py
+++ tests/test_math_funcs.py
@@ -32,21 +32,6 @@ def test_norm_ratio():
     assert np.isnan(mf.norm_ratio(0, 5))
 
 
-def test_normalize_array():
-
-    for i in range(10):
-
-        dim = np.random.randint(2, 5)  # Vector dimension
-        v = np.random.uniform(0, 10, dim)
-
-        normalized = mf.normalize_array(v)
-
-        in_range = np.logical_and(np.all(normalized >= 0),
-                                  np.all(normalized <= 1))
-
-        assert in_range
-
-
 def test_centre_of_mass():
 
     points = np.array([[0, 1], [0, -1]])
--- tests/test_signals.py
+++ tests/test_signals.py
@@ -10,3 +10,17 @@ def test_root_mean_square():
     x = np.array([0, 1, 2, 3])
     assert np.isclose(sig.root_mean_square(x), np.sqrt(14 / 4))
 
+
+def test_normalize():
+
+    for i in range(10):
+
+        dim = np.random.randint(2, 5)  # Vector dimension
+        v = np.random.uniform(0, 10, dim)
+
+        normalized = sig.normalize(v)
+
+        in_range = np.logical_and(np.all(normalized >= 0),
+                                  np.all(normalized <= 1))
+
+        assert in_range
