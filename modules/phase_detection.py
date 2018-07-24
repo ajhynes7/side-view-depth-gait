52c462ba60d59f18e07e800c10272ab95fc37f6e
Andrew Hynes <andrewhynes@ymail.com>
2018-07-24 17:25:36 -0400

    Change function detect_phases.

--- modules/phase_detection.py
+++ modules/phase_detection.py
@@ -82,48 +82,36 @@ def get_step_signal(direction_pass, foot_series_pass):
     return step_signal
 
 
-def detect_phases(step_signal, frames_interest):
+def detect_phases(foot_points, direction_pass):
     """
-    Return the phase (stance/swing) of each frame in a walking pass.
+    Detect the stance/swing phases of a foot during a walking pass.
 
     Parameters
     ----------
-    step_signal : Series
-        Signal with multiple steps.
-        Index values are frames.
-    frames_interest : ndarray
-        Sorted array of frames.
+    foot_points : ndarray
+        (n, 3) array of n foot positions.
+    direction_pass : ndarray
+        Vector for direction of a walking pass.
 
     Returns
     -------
-    frame_phases : Series
-        Indicates the walking phase of the corresponding frames.
-        Each element is either 'stance' or 'swing'.
+    is_stance : ndarray
+        (n, ) array of boolean values.
+        Element is True if the corresponding foot is in the stance phase.
 
     """
-    frames = step_signal.index.values
+    step_signal = lin.line_coordinate_system(np.zeros(3),
+                                             direction_pass, foot_points)
 
-    split_labels = nf.label_by_split(frames, frames_interest)
-    sub_signals = [*nf.group_by_label(step_signal.values, split_labels)]
-
-    variances = [*map(np.var, sub_signals)]
-    variance_array = np.array(variances).reshape(-1, 1)
+    variances = sw.apply_to_padded(step_signal, np.nanvar, r=5)
+    variance_array = nf.to_column(variances)
 
     k_means = KMeans(n_clusters=2, random_state=0).fit(variance_array)
-    variance_labels = k_means.labels_
-
-    sub_signal_lengths = [*map(len, sub_signals)]
-    expanded_labels = [*itf.repeat_by_list(variance_labels,
-                                           sub_signal_lengths)]
 
     stance_label = np.argmin(k_means.cluster_centers_)
-    swing_label = 1 - stance_label
-    phase_dict = {stance_label: 'stance', swing_label: 'swing'}
+    is_stance = k_means.labels_ == stance_label
 
-    phase_strings = itf.map_with_dict(expanded_labels, phase_dict)
-    frame_phases = pd.Series(phase_strings, index=frames)
-
-    return frame_phases
+    return is_stance
 
 
 def get_phase_dataframe(frame_phases):
