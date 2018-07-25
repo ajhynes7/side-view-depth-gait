dbc911200d8bd33019a8130c707e115245381e31
Andrew Hynes <andrewhynes@ymail.com>
2018-07-25 17:16:47 -0400

    Write generic function for testing either best fit line or plane.

--- tests/test_linear_algebra.py
+++ tests/test_linear_algebra.py
@@ -15,12 +15,24 @@ import modules.linear_algebra as lin
 floats = st.floats(min_value=-1e6, max_value=1e6)
 ints = st.integers(min_value=-1e6, max_value=1e6)
 
+n_points = st.one_of(st.integers(min_value=2, max_value=10))
+
 non_zero_vector = st.lists(ints, min_size=2, max_size=5).filter(lambda x:
                                                                 any(x))
 
+shapes = st.tuples(st.integers(min_value=2, max_value=10),
+                   st.integers(min_value=2, max_value=10))
+
+# Strategy for generating shapes of numpy arrays with dimension 2 or 3
+shapes_2_3 = st.tuples(st.integers(min_value=2, max_value=10),
+                       st.integers(min_value=2, max_value=3))
+
 cross_vector = st.lists(ints, min_size=3, max_size=3).filter(lambda x: any(x))
 
-array_3 = arrays('int', (3,), ints)
+point_3 = arrays('int', (3,), ints)
+
+points = arrays('int', shapes, ints)
+points_2_3 = arrays('int', shapes_2_3, ints)
 
 
 @given(non_zero_vector)
@@ -64,7 +76,7 @@ def test_parallel(u, v):
 
 @given(cross_vector, cross_vector, cross_vector)
 def test_collinear(point_a, point_b, point_c):
-
+    """Test function that checks for collinearity."""
     dist_ab = norm(np.subtract(point_a, point_b))
     dist_bc = norm(np.subtract(point_b, point_c))
     dist_ac = norm(np.subtract(point_a, point_c))
@@ -96,7 +108,7 @@ def test_closest_point():
     assert close_index == 3
 
 
-@given(array_3, array_3, array_3)
+@given(point_3, point_3, point_3)
 def test_project_point_line(point_p, point_a, point_b):
     """Tests for projecting a point onto a line."""
     if norm(point_a - point_b) == 0:
@@ -111,15 +123,15 @@ def test_project_point_line(point_p, point_a, point_b):
         vector_ab = np.subtract(point_a, point_b)
         vector_proj = np.subtract(point_p, point_proj)
 
-        assert lin.is_collinear(point_a, point_b, point_proj, atol=1e-3)
-        assert lin.is_perpendicular(vector_ab, vector_proj, atol=1e-3)
+        assert lin.is_collinear(point_a, point_b, point_proj, atol=0.1)
+        assert lin.is_perpendicular(vector_ab, vector_proj, atol=0.1)
 
         # The order of the line points should not matter
         point_proj_2 = lin.project_point_line(point_p, point_b, point_a)
         assert(np.allclose(point_proj, point_proj_2))
 
 
-@given(array_3, array_3, array_3)
+@given(point_3, point_3, point_3)
 def test_project_point_plane(point, point_plane, normal):
     """Tests for projecting a point onto a plane."""
     if norm(normal) == 0:
@@ -146,6 +158,23 @@ def test_project_point_plane(point, point_plane, normal):
                                         atol=0.1)
 
 
+@given(points_2_3, st.sampled_from([lin.best_fit_line, lin.best_fit_plane]))
+def test_best_fit(points, best_fit_func):
+    """Tests for the line of best fit in multidimensional space."""
+    centroid, direction = best_fit_func(points)
+
+    points_reversed = np.flip(points, axis=0)
+    centroid_rev, direction_rev = best_fit_func(points_reversed)
+
+    assert np.allclose(centroid, centroid_rev)
+    assert np.isclose(norm(direction), 1)
+
+    assert lin.is_parallel(direction, direction_rev)
+
+
+"""Parameterized tests"""
+
+
 @pytest.mark.parametrize("test_input, expected", [
     (np.array([1, 1, 0]), 'straight'),
     (np.array([-1, 5, 0]), 'straight'),
@@ -154,67 +183,52 @@ def test_project_point_plane(point, point_plane, normal):
     (np.array([4, 2, 1]), 'left'),
 ])
 def test_target_side(test_input, expected):
-
+    """Test specific examples of determining the side of a target."""
     forward = np.array([1, 0, 0])
     up = np.array([0, 1, 0])
 
     assert lin.target_side(test_input, forward, up) == expected
 
 
-# def test_best_fit_line_1():
+@pytest.mark.parametrize("points, centroid, direction", [
+    (np.array([[0, 0, 0], [1, 0, 0], [2, 0, 0]]), np.array([1, 0, 0]),
+     np.array([1, 0, 0])),
+    (np.array([[0, 0], [4, 0]]), np.array([2, 0]), np.array([1, 0])),
+    (np.array([[0, 0], [0, -10]]), np.array([0, -5]), np.array([0, -1])),
+])
+def test_best_fit_line_examples(points, centroid, direction):
+    """Test specific examples of best fit line."""
+    centroid_calc, direction_calc = lin.best_fit_line(points)
 
-#     points = np.random.rand(10, 3)
-#     _, direction = lin.best_fit_line(points)
-
-#     points_reversed = np.flip(points, axis=0)
-
-#     _, direction_reversed = lin.best_fit_line(points_reversed)
-
-#     # The two vectors should be parallel
-#     cross_prod = np.cross(direction_reversed, direction)
-#     npt.assert_array_almost_equal(cross_prod, np.array([0, 0, 0]))
-
-#     npt.assert_allclose(norm(direction), 1)
+    assert np.allclose(centroid, np.round(centroid_calc, 2))
+    assert np.allclose(direction, direction_calc)
 
 
-# @pytest.mark.parametrize("points, centroid, direction", [
-#     (np.array([[0, 0, 0], [1, 0, 0], [2, 0, 0]]), np.array([1, 0, 0]),
-#      np.array([1, 0, 0])),
-#     (np.array([[0, 0], [4, 0]]), np.array([2, 0]), np.array([1, 0])),
-#     (np.array([[0, 0], [0, -10]]), np.array([0, -5]), np.array([0, -1])),
-# ])
-# def test_best_fit_line_2(points, centroid, direction):
+@pytest.mark.parametrize("points, centroid, normal", [
+    (np.array([[0, 0, 0], [1, 0, 0], [0, 0, 1]]), np.array([0.33, 0, 0.33]),
+     np.array([0, -1, 0])),
+    (np.array([[0, 0, 0], [3, 0, 0], [0, 3, 0]]), np.array([1, 1, 0]),
+     np.array([0, 0, 1])),
+])
+def test_best_fit_plane_examples(points, centroid, normal):
+    """Test specific examples of best fit plane."""
+    centroid_calc, normal_calc = lin.best_fit_plane(points)
 
-#     centroid_calc, direction_calc = lin.best_fit_line(points)
-
-#     npt.assert_allclose(centroid, np.round(centroid_calc, 2))
-#     npt.assert_allclose(direction, direction_calc)
-
-
-# @pytest.mark.parametrize("points, centroid, normal", [
-#     (np.array([[0, 0, 0], [1, 0, 0], [0, 0, 1]]), np.array([0.33, 0, 0.33]),
-#      np.array([0, -1, 0])),
-#     (np.array([[0, 0, 0], [3, 0, 0], [0, 3, 0]]), np.array([1, 1, 0]),
-#      np.array([0, 0, 1])),
-# ])
-# def test_best_fit_plane(points, centroid, normal):
-
-#     centroid_calc, normal_calc = lin.best_fit_plane(points)
-
-#     npt.assert_allclose(centroid, np.round(centroid_calc, 2))
-#     npt.assert_allclose(normal, normal_calc)
+    assert np.allclose(centroid, np.round(centroid_calc, 2))
+    assert np.allclose(normal, normal_calc)
 
 
-# @pytest.mark.parametrize("a, b, expected", [
-#     (np.array([2, 0]), np.array([-2, 0]), np.pi),
-#     (np.array([5, 5, 5]), np.array([1, 1, 1]), 0),
-#     (np.array([1, 0]), np.array([1, 1]), np.pi / 4),
-#     (np.array([1, 0]), np.array([-5, -5]), 3 * np.pi / 4),
-# ])
-# def test_angle_between(a, b, expected):
+@pytest.mark.parametrize("a, b, expected", [
+    (np.array([2, 0]), np.array([-2, 0]), np.pi),
+    (np.array([5, 5, 5]), np.array([1, 1, 1]), 0),
+    (np.array([1, 0]), np.array([1, 1]), np.pi / 4),
+    (np.array([1, 0]), np.array([-5, -5]), 3 * np.pi / 4),
+])
+def test_angle_between_examples(a, b, expected):
+    """Test specific examples of the angle between two vectors."""
+    angle = lin.angle_between(a, b)
 
-#     angle = lin.angle_between(a, b)
+    assert np.allclose(angle, expected)
 
-#     npt.assert_allclose(angle, expected)
 
 points = [[1, 2, 3], [2, 2, 5], [-1, 10, 2], [2, 3, 5]]
