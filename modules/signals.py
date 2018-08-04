1983078965964fa27da547bc15954b104cd93c86
Andrew Hynes <andrewhynes@ymail.com>
2018-08-04 17:49:40 -0400

    Write property tests for signals.py

--- modules/signals.py
+++ modules/signals.py
@@ -3,13 +3,13 @@
 import numpy as np
 
 
-def root_mean_square(x):
+def root_mean_square(array):
     """
     Return the root mean square of an array.
 
     Parameters
     ----------
-    x : ndarray
+    array : ndarray
         Input array.
 
     Returns
@@ -19,21 +19,21 @@ def root_mean_square(x):
 
     Examples
     --------
-    >>> x = np.array([0, 1])
-    >>> root_mean_square(x) == np.sqrt(2) / 2
+    >>> array = np.array([0, 1])
+    >>> root_mean_square(array) == np.sqrt(2) / 2
     True
 
     """
-    return np.sqrt(sum(x**2) / x.size)
+    return np.sqrt(sum(array ** 2) / array.size)
 
 
-def normalize(x):
+def normalize(array):
     """
     Map all values in an array to the range [0, 1].
 
     Parameters
     ----------
-    x : array_like
+    array : array_like
         Input array.
         Max and min values should be different to avoid division by zero.
 
@@ -42,14 +42,27 @@ def normalize(x):
     ndarray
         Normalized array.
 
+    Raises
+    ------
+    ValueError
+        When minimum and maximum values are the same.
+
     Examples
     --------
-    >>> x = [i for i in range(5)]
-    >>> np.array_equal(normalize(x), [0, 0.25, 0.5, 0.75, 1])
+    >>> array = [i for i in range(5)]
+    >>> np.array_equal(normalize(array), [0, 0.25, 0.5, 0.75, 1])
     True
 
-    """
-    max_value = np.nanmax(x)
-    min_value = np.nanmin(x)
+    >>> normalize([1, 2, np.nan, 3])
+    array([0. , 0.5, nan, 1. ])
 
-    return (x - min_value) / (max_value - min_value)
+    """
+    max_value = np.nanmax(array)
+    min_value = np.nanmin(array)
+
+    array_range = max_value - min_value
+
+    if array_range == 0:
+        raise ValueError("Division by zero.")
+
+    return (array - min_value) / array_range
--- tests/test_signals.py
+++ tests/test_signals.py
@@ -1,27 +1,37 @@
+"""Test functions for signal processing."""
+
+import hypothesis.strategies as st
 import numpy as np
+from hypothesis import assume, given
+from hypothesis.extra.numpy import arrays
 
 import modules.signals as sig
 
+ints = st.integers(min_value=-1e6, max_value=1e6)
 
-def test_root_mean_square():
+# Generates either an integer or a nan
+ints_or_nan = st.one_of(ints, st.just(np.nan))
 
-    x = np.array([0, 1, 2])
-    assert np.isclose(sig.root_mean_square(x), np.sqrt(5 / 3))
-
-    x = np.array([0, 1, 2, 3])
-    assert np.isclose(sig.root_mean_square(x), np.sqrt(14 / 4))
+array_length = st.integers(min_value=1, max_value=50)
+array_1d = arrays('int', (array_length), ints)
 
 
-def test_normalize():
+@given(array_1d)
+def test_root_mean_square(array):
+    """Test that rms is at least zero and between the min and max."""
+    rms = sig.root_mean_square(array)
 
-    for i in range(10):
+    assert rms >= 0
+    assert np.min(array) <= rms <= np.max(np.abs(array))
 
-        dim = np.random.randint(2, 5)  # Vector dimension
-        v = np.random.uniform(0, 10, dim)
 
-        normalized = sig.normalize(v)
+@given(st.lists(ints_or_nan, min_size=1, max_size=50))
+def test_normalize(array):
+    """Test that all real values are in range [0, 1]."""
+    assume(~np.all(np.isnan(array)))
+    assume(np.nanmax(array) - np.nanmin(array) > 0)
 
-        in_range = np.logical_and(np.all(normalized >= 0),
-                                  np.all(normalized <= 1))
+    normalized = sig.normalize(array)
+    real_values = normalized[~np.isnan(normalized)]
 
-        assert in_range
+    assert np.logical_and(np.all(real_values >= 0), np.all(real_values <= 1))
