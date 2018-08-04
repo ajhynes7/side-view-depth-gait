748aa03809fe90100b2cd8266bd6a0a46f2013eb
Andrew Hynes <andrewhynes@ymail.com>
2018-08-04 18:08:20 -0400

    Remove unnecessary functions from numpy_funcs.

--- modules/numpy_funcs.py
+++ modules/numpy_funcs.py
@@ -1,7 +1,6 @@
 """Functions related to numpy arrays or operations."""
 
 import numpy as np
-from scipy.interpolate import interp1d
 
 import modules.iterable_funcs as itf
 
@@ -114,98 +113,6 @@ def all_consecutive(array):
     return np.all(np.diff(array) == 1)
 
 
-def is_sorted(array):
-    """
-    Check if array is sorted in ascending order.
-
-    Parameters
-    ----------
-    array : array_like
-        Input array.
-
-    Returns
-    -------
-    bool
-        True if array is in ascending order.
-
-    Examples
-    --------
-    >>> is_sorted([1, 2, 3])
-    True
-
-    >>> is_sorted([-10, -5, 0, 8])
-    True
-
-    >>> is_sorted([-5, 0, 4, 3, 6, 8])
-    False
-
-    """
-    return np.all(np.diff(array) >= 0)
-
-
-def divide_no_error(a, b):
-    """
-    Divide without allowing any floating-point errors.
-
-    Parameters
-    ----------
-    a, b : {int, float}
-        Input values
-
-    Returns
-    -------
-    float
-        Result of division
-
-    Examples
-    --------
-    >>> divide_no_error(10, 2)
-    5.0
-
-    >>> divide_no_error(10, 0)
-    Traceback (most recent call last):
-    ZeroDivisionError: division by zero
-
-    """
-    np.seterr(all='raise')
-
-    return a / b
-
-
-def dict_to_array(d):
-    """
-    Convert a dictionary to a numpy array.
-
-    Each dictionary key is an index to the array.
-
-    Parameters
-    ----------
-    d : Input dictionary.
-        All keys must be ints.
-
-    Returns
-    -------
-    x : ndarray
-        1-D numpy array.
-
-    Examples
-    --------
-    >>> d = {0: 1, 1: 1, 2: 2, 3: 4, 4: 5, 5: 10, 8: 20}
-
-    >>> dict_to_array(d)
-    array([ 1.,  1.,  2.,  4.,  5., 10., nan, nan, 20.])
-
-    """
-    n_elements = max(d.keys()) + 1
-
-    x = np.full(n_elements, np.nan)
-
-    for element in d:
-        x[element] = d[element]
-
-    return x
-
-
 def unique_no_sort(x):
     """
     Return array of unique elements with order preserved.
@@ -269,37 +176,6 @@ def map_sort(x):
     return itf.map_with_dict(x, mapping)
 
 
-def find_indices(array, elems_to_find):
-    """
-    Return the indices of array elements that match those in another array.
-
-    Parameters
-    ----------
-    array : array_like
-        Input array.
-    elems_to_find : array_like
-        Array of elements to find in the first array.
-
-    Returns
-    -------
-    ndarray
-        Indices to array.
-
-    Examples
-    --------
-    >>> array = [1, 2, 3, 5, 10]
-    >>> elems_to_find = [2, 3, 15]
-
-    >>> find_indices(array, elems_to_find)
-    array([1, 2])
-
-    >>> find_indices(array, [8, 11])
-    array([], dtype=int64)
-
-    """
-    return np.nonzero(np.in1d(array, elems_to_find))[0]
-
-
 def group_by_label(array, labels):
     """
     Group elements of an array by their corresponding labels.
@@ -337,6 +213,37 @@ def group_by_label(array, labels):
     """
     for label in np.unique(labels):
         yield array[labels == label]
+
+
+def find_indices(array, elems_to_find):
+    """
+    Return the indices of array elements that match those in another array.
+
+    Parameters
+    ----------
+    array : array_like
+        Input array.
+    elems_to_find : array_like
+        Array of elements to find in the first array.
+
+    Returns
+    -------
+    ndarray
+        Indices to array.
+
+    Examples
+    --------
+    >>> array = [1, 2, 3, 5, 10]
+    >>> elems_to_find = [2, 3, 15]
+
+    >>> find_indices(array, elems_to_find)
+    array([1, 2])
+
+    >>> find_indices(array, [8, 11])
+    array([], dtype=int64)
+
+    """
+    return np.nonzero(np.in1d(array, elems_to_find))[0]
 
 
 def label_by_split(array, split_vals):
@@ -382,46 +289,6 @@ def label_by_split(array, split_vals):
         labels[np.in1d(array, sub_array)] = i
 
     return labels
-
-
-def filter_consecutive_true(bool_array, min_length=2):
-    """
-    Remove sequences of consecutive True values that are too short.
-
-    Consecutive sequences of True with a length less than the specified minimum
-    are set to False.
-
-    Parameters
-    ----------
-    bool_array : array_like
-        Array of booleans.
-    min_length : int, optional
-        Minimum allowed length of consecutive True values (default 2).
-
-    Returns
-    -------
-    filt_array : ndarray
-        Filtered array with short groups of consecutive True set to False.
-
-    Examples
-    --------
-    >>> array = [True, True, True, False, True, True, False, False, True]
-
-    >>> filter_consecutive_true(array, min_length=3)
-    array([ True,  True,  True, False, False, False, False, False, False])
-
-    """
-    filt_array = np.array(bool_array)
-
-    group_labels = np.fromiter(itf.label_repeated_elements(bool_array), 'int')
-    unique_labels, unique_counts = np.unique(group_labels, return_counts=True)
-
-    for label, count in zip(unique_labels, unique_counts):
-
-        if np.all(filt_array[group_labels == label]) and count < min_length:
-            filt_array[group_labels == label] = False
-
-    return filt_array
 
 
 def large_boolean_groups(array_bool, labels, min_length=2):
@@ -558,83 +425,3 @@ def expand_arrays(x, y):
     y_expanded[index] = y
 
     return x_expanded, y_expanded
-
-
-def fill_with_previous(array):
-    """
-    Fill nan values in an array with the most recent non-nan value.
-
-    Parameters
-    ----------
-    array : array_like
-        Input array.
-
-    Returns
-    -------
-    array_filled : array_like
-        Array with nan values filled.
-
-    Examples
-    --------
-    >>> fill_with_previous([0, 0, np.nan, 1, 1, 2, np.nan, 3])
-    [0, 0, 0, 1, 1, 2, 2, 3]
-
-    >>> fill_with_previous([np.nan, 0, 1, 2])
-    [nan, 0, 1, 2]
-
-    """
-    array_filled = array.copy()
-    previous_number = np.nan
-
-    is_nan = np.isnan(array)
-
-    for i, value in enumerate(array):
-
-        if is_nan[i]:
-            array_filled[i] = previous_number
-        else:
-            previous_number = value
-
-    return array_filled
-
-
-def interp_nan(array):
-    """
-    Fill nan values of a 1D array using linear interpolation.
-
-    Parameters
-    ----------
-    array : ndarray
-        (n, ) input array.
-
-    Returns
-    -------
-    ndarray
-        (n, ) array with interpolated values.
-
-    Examples
-    --------
-    >>> interp_nan(np.array([1, np.nan, np.nan, np.nan, 3]))
-    array([1. , 1.5, 2. , 2.5, 3. ])
-
-    >>> array = np.array([1, 1, 3, np.nan, np.nan, 5, np.nan, 8, 10])
-    >>> np.round(interp_nan(array), 2)
-    array([ 1.  ,  1.  ,  3.  ,  3.67,  4.33,  5.  ,  6.5 ,  8.  , 10.  ])
-
-    >>> interp_nan(np.array([1, 2, 3]))
-    array([1., 2., 3.])
-
-    NaN values on the ends are not interpolated.
-
-    >>> interp_nan(np.array([np.nan, 1, 2, 3, np.nan]))
-    array([nan,  1.,  2.,  3., nan])
-
-    """
-    not_nan = ~np.isnan(array)
-
-    indices = np.arange(len(array))
-
-    x, y = indices[not_nan], array[not_nan]
-    f = interp1d(x, y, bounds_error=False)
-
-    return np.where(not_nan, array, f(indices))
