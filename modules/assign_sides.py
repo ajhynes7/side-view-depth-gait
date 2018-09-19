7a4dac41f6dc36b1b4cd336707df9595aa2aa0fb
Andrew Hynes <andrewhynes@ymail.com>
2018-09-19 19:06:16 -0400

    Group numpy imports.

--- modules/assign_sides.py
+++ modules/assign_sides.py
@@ -1,8 +1,8 @@
 """Functions for assigning correct sides to the feet."""
 
 import numpy as np
-import pandas as pd
 from numpy.linalg import norm
+import pandas as pd
 
 import modules.linear_algebra as lin
 import modules.numpy_funcs as nf
