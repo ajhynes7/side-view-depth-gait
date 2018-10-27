0998f78bcd48940d4ff0f8b8b88f93335e43b888
Andrew Hynes <andrewhynes@ymail.com>
2018-10-26 20:27:58 -0400

    Modify results script.

--- scripts/results.py
+++ scripts/results.py
@@ -6,7 +6,7 @@ import pandas as pd
 from scipy.stats import spearmanr, pearsonr
 
 import analysis.stats as st
-import modules.pandas_funcs as pf
+
 
 results_dir = os.path.join('data', 'results')
 match_dir = os.path.join('data', 'matching')
@@ -40,31 +40,30 @@ df_k = df_k.rename(columns=lambda x: str(x)[:-2])
 df_z = df_z.rename(columns=lambda x: str(x)[:-2])
 
 # Group by gait metric without suffix
-df_k_grouped = df_k.groupby(lambda x: x[:-2], axis=1).mean()
-df_z_grouped = df_z.groupby(lambda x: x[:-2], axis=1).mean()
+df_k_grouped = df_k.groupby(
+    lambda col_name: col_name[:-2],
+    axis=1).apply(lambda x: pd.Series(x.values.flatten('F')))
+
+df_z_grouped = df_z.groupby(
+    lambda col_name: col_name[:-2],
+    axis=1).apply(lambda x: pd.Series(x.values.flatten('F')))
 
 # Calculate results
-
 funcs = {
-    'pearson': lambda a, b: pearsonr(a, b)[0],
-    'spearman': lambda a, b: spearmanr(a, b)[0],
+    'pearson':
+    lambda a, b: pearsonr(a, b)[0],
+    'spearman':
+    lambda a, b: spearmanr(a, b)[0],
     'abs_rel_error':
     lambda a, b: st.relative_error(a, b, absolute=True).mean(),
-    'bias': lambda a, b: st.BlandAltman(a, b).bias,
-    'range': lambda a, b: st.BlandAltman(a, b).range
+    'bias':
+    lambda a, b: st.bland_altman(a, b).bias,
+    'range':
+    lambda a, b: st.bland_altman(a, b).range_,
 }
 
-dict_results_LR = {
-    name: pf.apply_to_columns(df_k, df_z, func)
-    for name, func in funcs.items()
-}
-dict_results_grouped = {
-    name: pf.apply_to_columns(df_k_grouped, df_z_grouped, func)
-    for name, func in funcs.items()
-}
-
-df_results_LR = pd.DataFrame(dict_results_LR).T
-df_results_grouped = pd.DataFrame(dict_results_grouped).T
+df_results_LR = st.compare_measurements(df_k, df_z, funcs)
+df_results_grouped = st.compare_measurements(df_k_grouped, df_z_grouped, funcs)
 
 df_results_LR.to_csv(os.path.join('results', 'results_LR.csv'))
 df_results_grouped.to_csv(os.path.join('results', 'results_grouped.csv'))
