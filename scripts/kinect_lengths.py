c3d02580581e697aae8fcbe12b17aa3915f238ca
Andrew Hynes <andrewhynes@ymail.com>
2018-11-12 17:46:20 -0500

    Change method in kinect_lengths to use 25% percentile
    Method is much faster than previous and uses no hyperparameters

--- scripts/kinect_lengths.py
+++ scripts/kinect_lengths.py
@@ -4,20 +4,21 @@ import glob
 import os
 import time
 
+import numpy as np
 import pandas as pd
+from scipy.spatial.distance import cdist
 
+import modules.iterable_funcs as itf
 import modules.pose_estimation as pe
 import modules.string_funcs as sf
 
 
 def cost_func(a, b):
     """Cost function for weighting edges of graph."""
-    return (a - b)**2
+    return (a - b) ** 2
 
 
 # Parameters
-
-n_frames_est = 60  # Number of frames used to estimate lengths on each trial
 
 lower_part_types = ['HEAD', 'HIP', 'UPPER_LEG', 'KNEE', 'LOWER_LEG', 'FOOT']
 
@@ -35,15 +36,22 @@ match_dir = os.path.join('data', 'matching')
 df_match = pd.read_csv(os.path.join(match_dir, 'match_kinect_zeno.csv'))
 
 # Create DataFrame of lengths between consecutive parts
-file_names = [os.path.splitext(os.path.basename(path))[0] for path in file_paths]
-df_lengths = pd.DataFrame(index=file_names, columns=range(len(lower_part_types) - 1))
+file_names = [os.path.splitext(os.path.basename(path))[0] for path in
+              file_paths]
+df_lengths = pd.DataFrame(index=file_names,
+                          columns=range(len(lower_part_types) - 1))
 df_lengths.index.name = 'File'
 
-# Select best positions from each Kinect data file
 t = time.time()
 total_frames = 0
 
-# Calculate lengths for each file
+n_lengths = len(lower_part_types) - 1
+
+# Pairs of consecutive body part indices
+index_pairs = list(itf.pairwise(range(len(lower_part_types))))
+
+
+# %% Calculate lengths for each file
 
 for file_path in file_paths:
 
@@ -60,27 +68,35 @@ for file_path in file_paths:
 
     df_lower = df[lower_parts].dropna(axis=0)
 
-    population_series = df_lower.apply(
-        lambda row: pe.get_population(row, part_labels)[0], axis=1)
+    n_frames = df_lower.shape[0]
+    total_frames += n_frames
 
-    label_series = df_lower.apply(
-        lambda row: pe.get_population(row, part_labels)[1], axis=1)
+    trial_lengths = np.zeros((n_frames, n_lengths))
 
-    # Estimate lengths between consecutive parts
-    lengths = pe.estimate_lengths(
-        population_series, label_series, cost_func, n_frames_est, eps=0.01)
+    for i, (_, frame_series) in enumerate(df_lower.iterrows()):
+
+        population, labels = pe.get_population(frame_series, part_labels)
+        dist_matrix = cdist(population, population)
+
+        for idx_a, idx_b in index_pairs:
+
+            pair_distances = dist_matrix[labels == idx_a][:, labels == idx_b]
+
+            trial_lengths[i, idx_a] = np.percentile(pair_distances, 25)
 
     # Fill in row of lengths DataFrame
-    df_lengths.loc[file_name] = lengths
+    df_lengths.loc[file_name] = np.median(trial_lengths, axis=0)
 
+df_lengths.to_csv(save_path)
+
+
+# %% Calculate run-time metrics
 
 n_trials = len(file_paths)
-total_frames = n_trials * n_frames_est
 
 time_elapsed = time.time() - t
 frames_per_second = round(total_frames / time_elapsed)
 
-df_lengths.to_csv(save_path)
 
 print("""
 Number of trials: {}\n
