d50928790b26718293b61c440a386774fdc8b896
Andrew Hynes <andrewhynes@ymail.com>
2018-11-19 15:47:00 -0500

    Add new scripts to run_all_results and fix bug in name==main

--- scripts/results/run_all_results.py
+++ scripts/results/run_all_results.py
@@ -1,22 +1,25 @@
-"""Run all result scripts."""
+"""Run all results scripts."""
 
 import scripts.results.bland_table as bland_table
 import scripts.results.combine_trials as combine_trials
 import scripts.results.length_table as length_table
 import scripts.results.plot_results as plot_results
+import scripts.results.process_ground_truth as process_ground_truth
+import scripts.results.validate_pose as validate_pose
 
 
 def main():
 
+    # Save dataframes
     combine_trials.main()
+    process_ground_truth.main()
 
-    # Plots
     plot_results.main()
 
-    # Tables
     bland_table.main()
     length_table.main()
+    validate_pose.main()
 
 
-if __name__ == 'main':
+if __name__ == '__main__':
     main()
