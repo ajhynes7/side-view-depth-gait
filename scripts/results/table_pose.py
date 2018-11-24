374bc09b43559fa64bed4985709a28f0a87f628a
Andrew Hynes <andrewhynes@ymail.com>
2018-11-24 18:31:18 -0500

    Delete alignment in results script for pose accuracy

--- scripts/results/table_pose.py
+++ scripts/results/table_pose.py
@@ -13,12 +13,11 @@ import modules.point_processing as pp
 
 def main():
 
-    # trial_name = '2014-12-03_P005_Post_002'
+    trial_name = '2014-12-03_P005_Post_002'
     # trial_name = '2014-12-03_P007_Post_000'
 
     best_pos_dir = join('data', 'kinect', 'best_pos')
     hypo_dir = join('data', 'kinect', 'processed', 'hypothesis')
-    align_dir = join('data', 'kinect', 'labelled_trials', 'alignment')
 
     # Max distance for accuracy calculation
     max_dist = 10
@@ -34,34 +33,13 @@ def main():
     # Position hypotheses (joint proposals)
     df_hypo = pd.read_pickle(join(hypo_dir, trial_name) + '.pkl')
 
-    # Used to align image file numbers and frame numbers
-    df_align = pd.read_csv(
-        join(align_dir, trial_name + '.txt'),
-        header=None,
-        names=['image_file'])
-
-    # Extract number from image file name
-    pattern = r'(\d+)\.png'
-    df_align['image_number'] = df_align.image_file.str.extract(pattern)
-
-    df_align = df_align.dropna()
-    df_align.image_number = pd.to_numeric(df_align.image_number)
-
-    df_hypo['image_number'] = df_align.image_number
-    df_hypo = df_hypo.set_index('image_number')
-
-    # Change index of selected positions to be the
-    # image numbers from the labelled trials.
-    # This allows for alignment with the ground truth
-    df_selected['image_number'] = df_align.image_number
-    df_selected = df_selected.set_index('image_number')
-
     df_compare = pd.concat(
         {
             'Selected': df_selected,
             'Truth': df_truth_trial,
             'Hypotheses': df_hypo,
-        }, axis=1).dropna()
+        },
+        axis=1).dropna()
 
     image_frames = df_compare.index.values
 
