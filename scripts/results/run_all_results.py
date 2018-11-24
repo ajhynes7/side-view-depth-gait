c647b0c9a7672de638a034d6b74fbfc3cf668ae8
Andrew Hynes <andrewhynes@ymail.com>
2018-11-24 18:28:19 -0500

    Refactor imports in results top script

--- scripts/results/run_all_results.py
+++ scripts/results/run_all_results.py
@@ -1,13 +1,8 @@
 """Run all results scripts."""
 
-import scripts.results.plot_labels as plot_labels
-import scripts.results.plot_results as plot_results
-import scripts.results.process_ground_truth as process_ground_truth
-import scripts.results.process_trials as process_trials
-import scripts.results.table_bland as table_bland
-import scripts.results.table_length_compare as table_length_compare
-import scripts.results.table_lengths as table_lengths
-import scripts.results.table_pose as table_pose
+from scripts.results import (plot_labels, plot_results, process_ground_truth,
+                             process_trials, table_bland, table_length_compare,
+                             table_lengths, table_pose)
 
 
 def main():
