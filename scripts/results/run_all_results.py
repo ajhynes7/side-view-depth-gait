68b26f626d9cfff75f746c706f63cd91f72845ae
Andrew Hynes <andrewhynes@ymail.com>
2018-11-24 15:33:55 -0500

    Refactor script to run all results

--- scripts/results/run_all_results.py
+++ scripts/results/run_all_results.py
@@ -1,22 +1,27 @@
 """Run all results scripts."""
 
-import scripts.results.bland_table as bland_table
-import scripts.results.combine_trials as combine_trials
-import scripts.results.length_table as length_table
+import scripts.results.plot_labels as plot_labels
 import scripts.results.plot_results as plot_results
 import scripts.results.process_ground_truth as process_ground_truth
+import scripts.results.process_trials as process_trials
+import scripts.results.table_bland as table_bland
+import scripts.results.table_length_compare as table_length_compare
+import scripts.results.table_lengths as table_lengths
+import scripts.results.table_pose as table_pose
 
 
 def main():
 
-    # Save dataframes
-    combine_trials.main()
+    process_trials.main()
     process_ground_truth.main()
 
     plot_results.main()
+    plot_labels.main()
 
-    bland_table.main()
-    length_table.main()
+    table_bland.main()
+    table_lengths.main()
+    table_length_compare.main()
+    table_pose.main()
 
 
 if __name__ == '__main__':
