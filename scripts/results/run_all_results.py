a083257a5d0395fbded096f7f4c4e48327d9b9c7
Andrew Hynes <andrewhynes@ymail.com>
2018-11-30 17:41:06 -0500

    Update run_all_results

--- scripts/results/run_all_results.py
+++ scripts/results/run_all_results.py
@@ -3,7 +3,7 @@
 import matplotlib.pyplot as plt
 
 from scripts.results import (plot_labels, plot_results, process_ground_truth,
-                             process_trials, table_bland, table_length_compare,
+                             process_trials, table_gait, table_length_compare,
                              table_lengths, table_pose)
 
 
@@ -14,16 +14,16 @@ def main():
     font = {'family': 'serif', 'weight': 'bold', 'size': 14}
     plt.rc('font', **font)
 
-    #process_trials.main()
     #process_ground_truth.main()
+    process_trials.main()
 
     #plot_labels.main()
     plot_results.main()
 
-    table_bland.main()
     table_lengths.main()
     table_length_compare.main()
     table_pose.main()
+    table_gait.main()
 
 
 if __name__ == '__main__':
