6da5be420214ef2fad6c2b8d05acf69c4d0c8663
Andrew Hynes <andrewhynes@ymail.com>
2019-02-11 12:30:35 -0500

    Delete unecessary functions and lines from analysis folder

--- analysis/math_funcs.py
+++ analysis/math_funcs.py
@@ -1,7 +1,5 @@
 """Math functions."""
 
-import math
-
 import numpy as np
 
 
@@ -33,74 +31,6 @@ def limits(x, tolerance):
     lower_lim, upper_lim = x - tolerance, x + tolerance
 
     return lower_lim, upper_lim
-
-
-def gaussian(x, sigma=1, mu=0):
-    """
-    Return a value from a Gaussian (normal) distribution.
-
-    Parameters
-    ----------
-    x : {int, float}
-        Main input value.
-    mu : {int, float}
-        Mean of distribution.
-    sigma : {int, float}
-        Standard deviation.
-
-    Returns
-    -------
-    float
-        Output of Gaussian function.
-
-    Examples
-    --------
-    >>> round(gaussian(0), 4)
-    0.3989
-
-    >>> round(gaussian(0, sigma=0.5), 4)
-    0.7979
-
-    >>> round(gaussian(-0.5, mu=1, sigma=0.5), 4)
-    0.0089
-
-    """
-    np.seterr(under='ignore')
-
-    coeff = 1.0 / np.sqrt(2 * np.pi * sigma**2)
-    exponent = np.exp(-(x - mu)**2 / (2 * sigma**2))
-
-    return coeff * exponent
-
-
-def sigmoid(x, a=1):
-    """
-    Sigmoid function (produces the sigmoid curve).
-
-    Parameters
-    ----------
-    x : {int, float}
-        Main argument to the function.
-
-    a : {int, float}, optional
-        Coefficient for x (default 1)
-        This changes the shape of the sigmoid curve.
-
-    Returns
-    -------
-    float
-        Output of sigmoid function.
-
-    Examples
-    --------
-    >>> sigmoid(0)
-    0.5
-
-    >>> round(sigmoid(1, a=2), 4)
-    0.8808
-
-    """
-    return 1 / (1 + math.exp(-a * x))
 
 
 def norm_ratio(a, b):
@@ -144,40 +74,3 @@ def norm_ratio(a, b):
         ratio = np.reciprocal(ratio)
 
     return ratio
-
-
-def centre_of_mass(points, masses):
-    """
-    Compute the centre of mass of a set of points.
-
-    Parameters
-    ----------
-    points : ndarray
-        List of points in space.
-    masses : ndarray
-        Mass of each point.
-
-    Returns
-    -------
-    ndarray
-        Centre of mass as a 1-D array.
-
-    Examples
-    --------
-    >>> points = np.array([[1, 2, 3], [-1, 2, 5], [3, 5, 7]])
-    >>> masses = np.array([1, 1, 1])
-
-    >>> centre_of_mass(points, masses)
-    array([1., 3., 5.])
-
-    >>> points = np.array([[-1, 10], [5, 2]])
-    >>> masses = np.array([0, 1])
-
-    >>> centre_of_mass(points, masses)
-    array([5., 2.])
-
-    """
-    num = np.sum((points.T * masses).T, axis=0)
-    denom = masses.sum()
-
-    return num / denom
--- analysis/stats.py
+++ analysis/stats.py
@@ -3,12 +3,11 @@
 from collections import namedtuple
 
 import numpy as np
-import pandas as pd
 
 import analysis.math_funcs as mf
 
 
-def relative_difference(x, y, absolute=False):
+def relative_difference(x, y):
     """
     Relative difference between values x and y.
 
@@ -18,9 +17,6 @@ def relative_difference(x, y, absolute=False):
     ----------
     x, y : {int, float, ndarray}
         Input values or arrays.
-    absolute : bool, optional
-        If True, the absolute relative error is returned
-        (the default is False).
 
     Returns
     -------
@@ -38,22 +34,14 @@ def relative_difference(x, y, absolute=False):
     >>> relative_difference(2, 3)
     -0.4
 
-    >>> relative_difference(2, 3, absolute=True)
-    0.4
-
     """
     difference = x - y
     mean_ = (x + y) / 2
 
-    rel_difference = difference / mean_
-
-    if absolute:
-        rel_difference = abs(rel_difference)
-
-    return rel_difference
+    return difference / mean_
 
 
-def relative_error(measured, actual, absolute=False):
+def relative_error(measured, actual):
     """
     Return the relative errors between measured and actual values.
 
@@ -67,10 +55,6 @@ def relative_error(measured, actual, absolute=False):
     actual : {int, float, ndarray}
         Actual value or array.
 
-    absolute : bool, optional
-        If True, the absolute relative error is returned
-        (the default is False).
-
     Returns
     -------
     error : {float, ndarray}
@@ -81,21 +65,14 @@ def relative_error(measured, actual, absolute=False):
     >>> relative_error(2, 5)
     -0.6
 
-    >>> relative_error(3, 5, absolute=True)
-    0.4
-
     >>> x = np.array([1, 2])
     >>> y = np.array([2, 2])
-    >>> relative_error(x, y, absolute=True)
-    array([0.5, 0. ])
+
+    >>> relative_error(x, y)
+    array([-0.5,  0. ])
 
     """
-    error = (measured - actual) / actual
-
-    if absolute:
-        error = abs(error)
-
-    return error
+    return (measured - actual) / actual
 
 
 def bland_altman(differences):
@@ -146,42 +123,7 @@ def bland_altman(differences):
         range_=upper_limit - lower_limit)
 
 
-def compare_measurements(df_1, df_2, compare_funcs):
-    """
-    Compare measurements taken by two devices.
-
-    Parameters
-    ----------
-    df_1, df_2 : DataFrame
-        Measurements of devices 1 and 2.
-        Columns are measurement names
-        Rows are individual measurements.
-    compare_funcs : dict
-        Keys are metric names, e.g., relative error
-        Values are functions of form column_1, column_2 -> scalar
-
-    Returns
-    -------
-    df_results : DataFrame
-        Columns are measurement names, e.g. stride length
-        Rows are metric names, e.g. relative error
-
-    """
-    measurements = df_1.columns
-    metrics = compare_funcs.keys()
-
-    df_results = pd.DataFrame(index=metrics, columns=measurements)
-
-    for measurement in measurements:
-        for metric, func in compare_funcs.items():
-
-            df_results.loc[metric, measurement] = func(df_1[measurement],
-                                                       df_2[measurement])
-
-    return df_results
-
-
-def icc(M, type=(2, 1)):
+def icc(M, type_icc=(2, 1)):
     """
     [summary]
 
