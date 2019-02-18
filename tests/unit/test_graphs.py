0a4579319653bbce679ec744f8402e5515208ca5
Andrew Hynes <andrewhynes@ymail.com>
2019-02-18 15:24:07 -0500

    Implement pytest fixtures for graph unit tests

--- tests/unit/test_graphs.py
+++ tests/unit/test_graphs.py
@@ -7,12 +7,61 @@ from numpy import nan
 import modules.graphs as gr
 
 
+@pytest.fixture
+def directed_acyclic_graph():
+    """Fixture for a directed acyclic graph."""
+    graph = {
+        0: {1: 2, 2: 5},
+        1: {3: 10, 2: 4},
+        2: {3: 3, 4: 1},
+        3: {4: 15, 5: 6},
+        4: {5: 3},
+        5: {},
+    }
+
+    order = [node for node in graph]
+
+    nodes_source = {0, 1}
+
+    return graph, order, nodes_source
+
+
+@pytest.fixture
+def label_graph():
+    """Fixture for converting labelled nodes into a graph."""
+
+    node_labels = {
+        0: 'head',
+        1: 'hip',
+        2: 'hip',
+        3: 'thigh',
+        4: 'thigh',
+        5: 'thigh',
+    }
+
+    label_adj_list = {
+        'head': {'hip': 60},
+        'hip': {'thigh': 15},
+        'thigh': {},
+    }
+
+    graph_expected = {
+        0: {1: 60, 2: 60},
+        1: {3: 15, 4: 15, 5: 15},
+        2: {3: 15, 4: 15, 5: 15},
+        3: {},
+        4: {},
+        5: {},
+    }
+
+    return node_labels, label_adj_list, graph_expected
+
+
 @pytest.mark.parametrize(
     "adj_list, matrix_expected",
     [
         ({0: {1: 2}}, np.array([[nan, 2], [nan, nan]])),
-        ({0: {1: 2}, 1: {0: -5, 1: 0}}, np.array([[nan, 2], [-5, 0]])),
-        (
+        ({0: {1: 2}, 1: {0: -5, 1: 0}}, np.array([[nan, 2], [-5, 0]])), (
             {0: {1: 2}, 1: {2: -5}, 2: {}},
             np.array([[nan, 2, nan], [nan, nan, -5], [nan, nan, nan]]),
         ),
@@ -44,54 +93,19 @@ def test_adj_list_to_matrix(adj_list, matrix_expected):
         (5, [1, 2, 4, 5]), 
     ],
 )
-def test_shortest_paths(node_target, path):
+def test_shortest_paths(directed_acyclic_graph, node_target, path):
+
+    graph, order, nodes_source = directed_acyclic_graph
 
     prev, _ = gr.dag_shortest_paths(graph, order, nodes_source)
     assert gr.trace_path(prev, node_target) == path
 
 
-def test_labelled_nodes_to_graph():
+def test_labelled_nodes_to_graph(label_graph):
 
-    assert gr.labelled_nodes_to_graph(node_labels, label_adj_list) == graph_parts
+    node_labels, label_adj_list, graph_expected = label_graph
+    
+    graph_from_labels = gr.labelled_nodes_to_graph(node_labels, label_adj_list)
 
-
-graph = {
-    0: {1: 2, 2: 5},
-    1: {3: 10, 2: 4},
-    2: {3: 3, 4: 1},
-    3: {4: 15, 5: 6},
-    4: {5: 3},
-    5: {},
-}
-
-nodes_source = {0, 1}
-order = [node for node in graph]
-
-
-label_adj_list = {
-    'head': {'hip': 60},
-    'hip': {'thigh': 15},
-    'thigh': {},
-}
-
-node_labels = {
-    0: 'head',
-    1: 'hip',
-    2: 'hip',
-    3: 'thigh',
-    4: 'thigh',
-    5: 'thigh',
-}
-
-graph_parts = {
-    0: {1: 60, 2: 60},
-    1: {3: 15, 4: 15, 5: 15},
-    2: {3: 15, 4: 15, 5: 15},
-    3: {},
-    4: {},
-    5: {},
-}
-
-
-
+    assert graph_from_labels == graph_expected
 
