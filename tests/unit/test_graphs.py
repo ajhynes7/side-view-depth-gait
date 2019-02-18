eb629bb79659f23c9fe207c873d83d1dacc75a9e
Andrew Hynes <andrewhynes@ymail.com>
2019-02-18 13:49:56 -0500

    Add unit tests for adjacency list conversion

--- tests/unit/test_graphs.py
+++ tests/unit/test_graphs.py
@@ -1,15 +1,38 @@
 """Unit tests for graph functions."""
 
+import numpy as np
 import pytest
+from numpy import nan
 
 import modules.graphs as gr
 
 
-@pytest.mark.parametrize("target_node, path", [
-    (5, [1, 2, 4, 5]),
-    (3, [1, 2, 3]),
-    (0, [0]),
-])
+@pytest.mark.parametrize(
+    "adj_list, matrix_expected",
+    [
+        ({0: {1: 2}}, np.array([[nan, 2], [nan, nan]])),
+        ({0: {1: 2}, 1: {0: -5, 1: 0}}, np.array([[nan, 2], [-5, 0]])),
+        (
+            {0: {1: 2}, 1: {2: -5}, 2: {}},
+            np.array([[nan, 2, nan], [nan, nan, -5], [nan, nan, nan]]),
+        ),
+    ],
+)
+def test_adj_list_to_matrix(adj_list, matrix_expected):
+
+    if len(adj_list) != len(matrix_expected):
+
+        with pytest.raises(Exception):
+            gr.adj_list_to_matrix(adj_list)
+
+    else:
+
+        adj_matrix = gr.adj_list_to_matrix(adj_list)
+
+        assert np.allclose(adj_matrix, matrix_expected, equal_nan=True)
+
+        assert gr.adj_matrix_to_list(adj_matrix) == adj_list
+
 def test_paths(target_node, path):
 
     prev, _ = gr.dag_shortest_paths(G, V, source_nodes)
