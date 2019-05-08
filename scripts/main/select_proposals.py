b4854971d777109bf03c6fbd3e83ba039d454ddf
Andrew Hynes <andrewhynes@ymail.com>
2019-05-07 22:07:57 -0400

    Update select_proposals to use new df_hypo

--- scripts/main/select_proposals.py
+++ scripts/main/select_proposals.py
@@ -1,7 +1,7 @@
 """Select the best body part positions from multiple joint proposals."""
 
-import os
 import time
+from os.path import join
 
 import numpy as np
 import pandas as pd
@@ -29,63 +29,35 @@ def main():
         [[0, 1], [1, 2], [2, 3], [3, 4], [4, 5], [3, 5], [1, 3]]
     )
 
-    part_labels = range(part_connections.max() + 1)
+    # DataFrame with position hypotheses (join proposals) for all trials
+    df_hypo = pd.read_pickle(join('data', 'kinect', 'df_hypo.pkl'))
 
-    # Reading data
-    load_dir = os.path.join('data', 'kinect', 'processed', 'hypothesis')
-    save_dir = os.path.join('data', 'kinect', 'best_pos')
+    n_frames_total = df_hypo.shape[0]
 
-    # DataFrame with lengths between body parts
-    length_path = os.path.join(
-        'data', 'kinect', 'lengths', 'kinect_lengths.csv'
-    )
+    # DataFrame with expected lengths between body parts
+    length_path = join('data', 'kinect', 'lengths', 'kinect_lengths.csv')
     df_length = pd.read_csv(length_path, index_col=0)
 
+    # Pre-allocate array to hold best head and foot positions
+    # on each frame
+    array_selected = np.full((n_frames_total, 3), fill_value=None)
+
     t = time.time()
-    trials_run, frames_run = 0, 0
+    index_row = 0
 
-    # %% Select best positions from each Kinect data file
+    for trial_name, df_trial in df_hypo.groupby(level=0):
 
-    # List of trials to run
-    running_path = os.path.join(
-        'data', 'kinect', 'running', 'trials_to_run.csv'
-    )
-    trials_to_run = pd.read_csv(running_path, header=None, squeeze=True).values
-
-    for trial_name in trials_to_run:
+        print(trial_name)  # Print current trial just to show progress
 
         lengths = df_length.loc[trial_name]  # Read estimated lengths for trial
-
-        file_path = os.path.join(load_dir, trial_name + '.pkl')
-
-        # Position hypotheses (joint proposals)
-        df_hypo = pd.read_pickle(file_path)
-
-        # Delete rows missing a body part type (head, hip, etc.)
-        df_hypo = df_hypo.dropna()
-
-        population_series = df_hypo.apply(
-            lambda row: pe.get_population(row, part_labels)[0], axis=1
-        )
-
-        label_series = df_hypo.apply(
-            lambda row: pe.get_population(row, part_labels)[1], axis=1
-        )
 
         # Expected lengths for all part connections,
         # including non-adjacent (e.g., knee to foot)
         label_adj_list = pe.lengths_to_adj_list(part_connections, lengths)
 
-        # Select paths to feet on each frame
+        for tuple_frame in df_trial.itertuples():
 
-        # List of image frames with data
-        frames = population_series.index.values
-
-        best_pos_list = []
-
-        for f in frames:
-            population = population_series.loc[f]
-            labels = label_series.loc[f]
+            population, labels = tuple_frame.population, tuple_frame.labels
 
             # Select the best two shortest paths
             pos_1, pos_2 = pe.process_frame(
@@ -97,43 +69,35 @@ def main():
                 score_func,
             )
 
-            best_pos_list.append((pos_1, pos_2))
+            # Positions of the best head and two feet
+            array_selected[index_row, 0] = pos_1[0, :]
+            array_selected[index_row, 1] = pos_1[-1, :]
+            array_selected[index_row, 2] = pos_2[-1, :]
 
-        df_best_pos = pd.DataFrame(
-            best_pos_list, index=frames, columns=['Side A', 'Side B']
-        )
+            index_row += 1
 
-        # Head and foot positions as series
-        head_pos = df_best_pos['Side A'].apply(lambda row: row[0, :])
-        foot_pos_1 = df_best_pos['Side A'].apply(lambda row: row[-1, :])
-        foot_pos_2 = df_best_pos['Side B'].apply(lambda row: row[-1, :])
+    # DataFrame of selected head and foot positions.
+    df_selected = pd.DataFrame(
+        array_selected,
+        index=df_hypo.index,
+        columns=['HEAD', 'A_FOOT', 'B_FOOT'],
+    )
 
-        # Combine into new DataFrame
-        df_head_feet = pd.concat([head_pos, foot_pos_1, foot_pos_2], axis=1)
-        df_head_feet.columns = ['HEAD', 'L_FOOT', 'R_FOOT']
-        df_head_feet.index.name = 'frame'
-
-        # Save data
-        save_path = os.path.join(save_dir, trial_name) + '.pkl'
-        df_head_feet.to_pickle(save_path)
-
-        trials_run += 1
-        frames_run += len(frames)
+    df_selected.to_pickle(join('data', 'kinect', 'df_selected.pkl'))
 
     # %% Calculate run-time metrics
 
     time_elapsed = time.time() - t
-    frames_per_second = np.round(frames_run / time_elapsed)
+
+    frames_per_second = round(n_frames_total / time_elapsed)
 
     print(
         """
-    Number of trials: {}\n
-    Number of frames: {}\n
-    Total time: {}\n
-    Frames per second: {}""".format(
-            trials_run,
-            frames_run,
-            np.round(time_elapsed, 2),
+        Number of frames: {}\n
+        Total time: {}\n
+        Frames per second: {}""".format(
+            n_frames_total,
+            round(time_elapsed, 2),
             frames_per_second,
         )
     )
