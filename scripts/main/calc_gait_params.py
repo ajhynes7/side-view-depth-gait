bb6fe6c1ab06cc99788efa725d881719d24de36f
Andrew Hynes <andrewhynes@ymail.com>
2019-05-08 13:31:20 -0400

    Update calc_gait_params to use best-fit lines and save to one dataframe

--- scripts/main/calc_gait_params.py
+++ scripts/main/calc_gait_params.py
@@ -1,5 +1,6 @@
 """Calculate gait parameters from Kinect data."""
 
+import pickle
 from os.path import join
 
 import pandas as pd
@@ -9,23 +10,33 @@ import modules.gait_parameters as gp
 
 def main():
 
-    kinect_dir = join('data', 'kinect')
-
+    # Table of matching Kinect and Zeno trials
     df_match = pd.read_csv(join('data', 'matching', 'match_kinect_zeno.csv'))
+
+    # DataFrame of head and assigned L/R foot positions
+    df_assigned = pd.read_pickle(join('data', 'kinect', 'df_assigned.pkl'))
+
+    # Lines of best fit for each walking pass of each trial
+    with open(join('data', 'kinect', 'dict_lines_fit.pkl'), 'rb') as handle:
+        dict_lines_fit = pickle.load(handle)
+
+    dict_gait = {}
 
     for trial_name in df_match.kinect:
 
-        df_assigned = pd.read_pickle(
-            join(kinect_dir, 'assigned', trial_name + '.pkl')
-        )
-        direction_series = pd.read_pickle(
-            join(kinect_dir, 'direction', trial_name + '.pkl')
-        )
+        print(trial_name)  # Print current trial just to show progress
 
-        df_trial = gp.combine_walking_passes(df_assigned, direction_series)
+        df_assigned_trial = df_assigned.loc[trial_name]
 
-        save_path = join(kinect_dir, 'gait_params', trial_name + '.pkl')
-        df_trial.to_pickle(save_path)
+        # Best fit lines for the trial (one for each walking pass)
+        lines_trial = dict_lines_fit[trial_name]
+
+        dict_gait[trial_name] = gp.combine_walking_passes(df_assigned_trial, lines_trial)
+
+    df_gait = pd.concat(dict_gait)
+
+    # Save the gait parameters for each trial
+    df_gait.to_pickle(join('data', 'kinect', 'df_gait.pkl'))
 
 
 if __name__ == '__main__':
