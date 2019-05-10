8c20750c9010f0547d66cc31dfca80978fce458c
Andrew Hynes <andrewhynes@ymail.com>
2019-05-10 00:43:36 -0400

    Update run_all_results with new scripts

--- scripts/results/run_all_results.py
+++ scripts/results/run_all_results.py
@@ -1,4 +1,4 @@
-"""Run all results scripts."""
+"""Run all results."""
 
 import matplotlib
 from matplotlib import rc
@@ -6,12 +6,13 @@ from matplotlib import rc
 from scripts.results import (
     align_frames,
     compare_gait,
+    compare_lengths,
+    compare_positions,
+    compare_radii,
+    group_lengths,
     match_trials,
     plot_accuracy_radii,
     process_ground_truth,
-    table_length_compare,
-    table_lengths,
-    table_pose,
 )
 
 
@@ -23,18 +24,23 @@ def main():
     rc('font', **{'family': 'serif', 'weight': 'bold', 'size': 14})
     rc('text', usetex=True)
 
-    # %% Comparison with labelled depth images
+    # %% Estimated lengths grouped by participant
+
+    group_lengths.main()
+
+    # %% Comparison with labelled images
 
     align_frames.main()
     process_ground_truth.main()
 
-    table_lengths.main()
-    table_length_compare.main()
-    table_pose.main()
+    compare_lengths.main()
+    compare_positions.main()
 
+    # Foot selection accuracy with different radii
+    compare_radii.main()
     plot_accuracy_radii.main()
 
-    # %% Comparison with Zeno Walkway
+    # %%  Comparison with Zeno Walkway
 
     match_trials.main()
     compare_gait.main()
