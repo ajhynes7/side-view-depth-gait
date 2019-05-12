65f2472055aee1b2493aa9e17eeedef568f9f5b0
Andrew Hynes <andrewhynes@ymail.com>
2019-05-11 23:26:41 -0400

    Fix path to images in copy_data

--- shell_scripts/copy_data.sh
+++ shell_scripts/copy_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp figures/* ../gait_journal/figures/
+cp figures/* ../gait_journal/images/
 
-cp results/plots/* ../gait_journal/figures/
+cp results/plots/* ../gait_journal/images/
 cp results/tables/* ../gait_journal/tables/
