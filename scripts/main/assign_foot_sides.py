f2859c8bbb1ecdabb65f85119fd6c0b9e3bea523
Andrew Hynes <andrewhynes@ymail.com>
2019-05-20 17:33:05 -0400

    Edit assignment script to save 2D and 3D foot points

--- scripts/main/assign_foot_sides.py
+++ scripts/main/assign_foot_sides.py
@@ -5,16 +5,21 @@ from os.path import join
 import numpy as np
 import pandas as pd
 
-import modules.assign_sides as asi
 import modules.pandas_funcs as pf
+import modules.point_processing as pp
+import modules.side_assignment as sa
 
 
 def main():
 
-    df_selected_passes = pd.read_pickle(join('data', 'kinect', 'df_selected_passes.pkl'))
+    kinect_dir = join('data', 'kinect')
+
+    df_selected_passes = pd.read_pickle(join(kinect_dir, 'df_selected_passes.pkl'))
 
     tuples_trial_pass = df_selected_passes.index.droplevel('frame').values
-    dict_assigned = dict.fromkeys(tuples_trial_pass)
+
+    dict_assigned_2d = dict.fromkeys(tuples_trial_pass)
+    dict_assigned_3d = dict.fromkeys(tuples_trial_pass)
 
     for tuple_trial_pass, df_pass in df_selected_passes.groupby(level=[0, 1]):
 
@@ -22,22 +27,37 @@ def main():
         frames = df_pass.index.values
 
         points_head = np.stack(df_pass.HEAD)
-        points_a = np.stack(df_pass.L_FOOT)
-        points_b = np.stack(df_pass.R_FOOT)
+        points_3d_a = np.stack(df_pass.L_FOOT)
+        points_3d_b = np.stack(df_pass.R_FOOT)
 
-        # Reduce the dimension of the foot points.
-        points_2d_a, points_2d_b = asi.convert_to_2d(points_head, points_a, points_b)
+        # Convert the 3D foot points to a 2D coordinate system.
+        points_2d_a, points_2d_b = sa.convert_to_2d(points_head, points_3d_a, points_3d_b)
 
-        # Assign left/right sides to the feet.
-        points_l, points_r = asi.assign_sides_pass(frames, points_2d_a, points_2d_b)
+        # Split the walking pass into portions between the feet coming together.
+        labels_portions = sa.split_walking_pass(frames, points_2d_a, points_2d_b)
 
-        dict_assigned[tuple_trial_pass] = pf.points_to_dataframe([points_l, points_r], ['L_FOOT', 'R_FOOT'], frames)
+        # Assign left and right sides to the 2D foot positions.
+        array_assignment = sa.assign_sides_pass(points_2d_a, points_2d_b, labels_portions)
 
-    df_assigned = pd.concat(dict_assigned)
-    df_assigned.index.names = ['trial_name', 'num_pass', 'frame']
+        points_3d_l, points_3d_r = pp.correspond_points(points_3d_a, points_3d_b, array_assignment)
+        points_2d_l, points_2d_r = pp.correspond_points(points_2d_a, points_2d_b, array_assignment)
+
+        df_2d = pf.points_to_dataframe([points_2d_l, points_2d_r], ['L_FOOT', 'R_FOOT'], frames)
+        df_3d = pf.points_to_dataframe([points_3d_l, points_3d_r], ['L_FOOT', 'R_FOOT'], frames)
+
+        dict_assigned_2d[tuple_trial_pass] = df_2d
+        dict_assigned_3d[tuple_trial_pass] = df_3d
+
+    df_assigned_2d = pd.concat(dict_assigned_2d)
+    df_assigned_3d = pd.concat(dict_assigned_2d)
+
+    index_names = ['trial_name', 'num_pass', 'frame']
+    df_assigned_2d.index.names = index_names
+    df_assigned_3d.index.names = index_names
 
     # Save the assigned positions for each trial
-    df_assigned.to_pickle(join('data', 'kinect', 'df_assigned.pkl'))
+    df_assigned_2d.to_pickle(join(kinect_dir, 'df_assigned_2d.pkl'))
+    df_assigned_3d.to_pickle(join(kinect_dir, 'df_assigned_3d.pkl'))
 
 
 if __name__ == '__main__':
