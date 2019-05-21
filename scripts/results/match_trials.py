4e8aa2f6d2fecc2c63e8f891062216a1fd3443bb
Andrew Hynes <andrewhynes@ymail.com>
2019-05-21 19:42:42 -0400

    Edit match_trials to record trial ID and trial name

--- scripts/results/match_trials.py
+++ scripts/results/match_trials.py
@@ -21,15 +21,18 @@ def main():
         trial_name_k = tuple_row.kinect
         trial_name_z = tuple_row.zeno
 
-        dict_k[trial_id] = df_gait_k.loc[trial_name_k]
-        dict_z[trial_id] = df_gait_z.loc[trial_name_z]
+        dict_k[(trial_id, trial_name_k)] = df_gait_k.loc[trial_name_k]
+        dict_z[(trial_id, trial_name_z)] = df_gait_z.loc[trial_name_z]
 
     # Gait parameters with matching trial IDs
-    df_trial_id_k = pd.concat(dict_k)
-    df_trial_id_z = pd.concat(dict_z)
+    df_matched_k = pd.concat(dict_k)
+    df_matched_z = pd.concat(dict_z)
 
-    df_trial_id_k.to_pickle(join('data', 'kinect', 'df_trial_id.pkl'))
-    df_trial_id_z.to_pickle(join('data', 'zeno', 'df_trial_id.pkl'))
+    df_matched_k.index = df_matched_k.index.rename(['trial_id', 'trial_name'], level=[0, 1])
+    df_matched_z.index = df_matched_z.index.rename(['trial_id', 'trial_name'], level=[0, 1])
+
+    df_matched_k.to_pickle(join('data', 'kinect', 'df_matched.pkl'))
+    df_matched_z.to_pickle(join('data', 'zeno', 'df_matched.pkl'))
 
 
 if __name__ == '__main__':
