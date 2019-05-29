893d6eec92cf8d8ecfc6293aa96cb1b747da8355
Andrew Hynes <andrewhynes@ymail.com>
2019-05-29 16:28:38 -0400

    Update script to show stance clusters

--- scripts/figures/stance_clusters.py
+++ scripts/figures/stance_clusters.py
@@ -5,7 +5,6 @@ from os.path import join
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from sklearn.cluster import DBSCAN
 from skspatial.transformation import transform_coordinates
 
 import modules.numpy_funcs as nf
@@ -13,6 +12,8 @@ import modules.phase_detection as pde
 
 
 def main():
+
+    # %% Load data
 
     trial_name, num_pass = '2014-12-08_P007_Post_003', 0
 
@@ -22,6 +23,8 @@ def main():
     points_head = np.stack(df_pass.HEAD)
     points_a = np.stack(df_pass.L_FOOT)
     points_b = np.stack(df_pass.R_FOOT)
+
+    # %% Cluster stance phases
 
     # Group points together by alternating from groups A and B.
     # This ensures that the points move in one general direction.
@@ -35,20 +38,21 @@ def main():
 
     # Convert foot points into new coordinates defined by forward, up, and perpendicular directions.
     points_transformed = transform_coordinates(points_foot_inlier, basis.origin, (basis.up, basis.perp, basis.forward))
-    coords_up, coords_perp, coords_forward = np.hsplit(points_transformed, 3)
+    coords_up, coords_perp, coords_forward = np.split(points_transformed, 3, 1)
 
-    labels = DBSCAN(eps=5, min_samples=10).fit(np.column_stack((coords_forward, coords_perp))).labels_
+    points_2d = np.column_stack((coords_perp, coords_forward))
+    labels_stance = pde.label_stance_phases(points_2d)
 
     # %% Plot figure
 
     x, y = coords_perp.flatten(), coords_forward.flatten()
-    is_noise = labels == -1
+    is_noise = labels_stance == -1
 
     fig = plt.figure()
 
     plt.scatter(x[is_noise], y[is_noise], c='k', s=10)
 
-    plt.scatter(x[~is_noise], y[~is_noise], c=labels[~is_noise], cmap='Set1', edgecolor='k', s=50)
+    plt.scatter(x[~is_noise], y[~is_noise], c=labels_stance[~is_noise], cmap='Set1', edgecolor='k', s=50)
 
     plt.xlabel('Perpendicular coordinates [cm]')
     plt.ylabel('Forward coordinates [cm]')
