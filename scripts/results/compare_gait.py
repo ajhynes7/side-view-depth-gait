2a81681ac2a465b3916c05790187669fe152d68f
Andrew Hynes <andrewhynes@ymail.com>
2019-05-30 12:09:42 -0400

    Add gait results for L/R sides and move Bland plots to separate script

--- scripts/results/compare_gait.py
+++ scripts/results/compare_gait.py
@@ -1,8 +1,7 @@
-"""Create plots and tables for comparing Kinect to Zeno Walkway."""
+"""Compare the gait parameters of the Kinect and Zeno Walkway."""
 
 from os.path import join
 
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
@@ -11,10 +10,7 @@ import analysis.stats as st
 
 def main():
 
-    dir_plots = join('results', 'plots')
-    dir_tables = join('results', 'tables')
-
-    # Gait parameters from all trials with matching IDs
+    # Gait parameters from all trials with matching IDs.
     df_matched_k = pd.read_pickle(join('data', 'kinect', 'df_matched.pkl'))
     df_matched_z = pd.read_pickle(join('data', 'zeno', 'df_matched.pkl'))
 
@@ -22,100 +18,78 @@ def main():
     assert df_matched_k.index.names == df_matched_z.index.names
     assert set(df_matched_k.columns) == set(df_matched_z.columns)
 
-    # Median gait parameters of each walking trial.
-    df_trials_k = df_matched_k.groupby(['trial_id']).median()
-    df_trials_z = df_matched_z.groupby(['trial_id']).median()
+    gait_params = df_matched_k.columns
 
-    gait_params = df_matched_k.columns.values
+    # %% Calculate results per trial.
 
-    list_bland_tuples = []
-    dict_icc = {f'icc_{x}1': {} for x in ['2', '3']}
+    df_trials_k = df_matched_k.groupby('trial_id').median()
+    df_trials_z = df_matched_z.groupby('trial_id').median()
+
+    dict_icc, dict_bland = {}, {}
 
     for param in gait_params:
 
         measures_k = df_trials_k[param]
         measures_z = df_trials_z[param]
+
         measures = np.column_stack((measures_k, measures_z))
 
-        means = (measures_k + measures_z) / 2
+        icc_21 = st.icc(measures, form=(2, 1))
+        icc_31 = st.icc(measures, form=(3, 1))
+
         differences = st.relative_difference(measures_k, measures_z)
         bland_alt = st.bland_altman(differences)
 
-        list_bland_tuples.append(bland_alt)
+        dict_icc[param] = {'ICC_21': icc_21, 'ICC_31': icc_31}
+        dict_bland[param] = bland_alt._asdict()
 
-        dict_icc['icc_21'][param] = st.icc(measures, form=(2, 1))
-        dict_icc['icc_31'][param] = st.icc(measures, form=(3, 1))
+    df_icc = pd.DataFrame.from_dict(dict_icc, orient='index')
+    df_bland = pd.DataFrame.from_dict(dict_bland, orient='index')
 
-        # %% Bland-Altman plot
+    # %% Calculate results for left and right sides.
 
-        fig_1, ax_1 = plt.subplots()
+    df_sides_k = df_matched_k.groupby(['trial_id', 'side']).median()
+    df_sides_z = df_matched_z.groupby(['trial_id', 'side']).median()
 
-        ax_1.scatter(means, differences, c='k', s=20)
+    dict_icc_sides, dict_bland_sides = {}, {}
 
-        # Horizontal lines for bias and limits of agreement
-        ax_1.axhline(y=bland_alt.bias, color='k', linestyle='-')
-        ax_1.axhline(y=bland_alt.lower_limit, color='k', linestyle=':')
-        ax_1.axhline(y=bland_alt.upper_limit, color='k', linestyle=':')
+    for side in ['L', 'R']:
 
-        # Enlarge y range
-        y_lims = np.array(ax_1.get_ylim())
-        y_range = y_lims[1] - y_lims[0]
-        ax_1.set_ylim(y_lims + 0.5 * y_range * np.array([-1, 1]))
+        df_side_k = df_sides_k[df_sides_k.index.get_level_values('side') == side]
+        df_side_z = df_sides_z[df_sides_z.index.get_level_values('side') == side]
 
-        # Reduce number of ticks on the axes, so that figure can be small
-        ax_1.locator_params(nbins=5)
+        for param in gait_params:
 
-        ax_1.tick_params(labelsize=25)  # Increase font of tick labels
+            measures_k = df_side_k[param]
+            measures_z = df_side_z[param]
 
-        # Format y labels as percentages.
-        # This line needs to be after the others,
-        # or else the y-axis may be incorrect.
-        ax_1.set_yticklabels([f'{(x * 100):.0f}' for x in ax_1.get_yticks()])
+            measures = np.column_stack((measures_k, measures_z))
 
-        # Remove right and top borders
-        ax_1.spines['right'].set_visible(False)
-        ax_1.spines['top'].set_visible(False)
+            icc_21 = st.icc(measures, form=(2, 1))
+            icc_31 = st.icc(measures, form=(3, 1))
 
-        plt.xlabel('Mean of two measurements [cm]', fontsize=30)
-        plt.ylabel(r'Relative difference [\%]', fontsize=30)
-        plt.tight_layout()
+            differences = st.relative_difference(measures_k, measures_z)
+            bland_alt = st.bland_altman(differences)
 
-        fig_1.savefig(join(dir_plots, 'bland_{}.png'.format(param)))
+            dict_icc_sides[(param, side)] = {'ICC_21': icc_21, 'ICC_31': icc_31}
+            dict_bland_sides[(param, side)] = bland_alt._asdict()
 
-        # %% Direct comparison plots
+    df_icc_sides = pd.DataFrame.from_dict(dict_icc_sides, orient='index').unstack()
+    df_bland_sides = pd.DataFrame.from_dict(dict_bland_sides, orient='index').unstack()
 
-        fig_2, ax_2 = plt.subplots()
+    # %% Save results as LaTeX tables.
 
-        ax_2.scatter(measures_z, measures_k, c='k', s=20)
+    dict_dfs = {
+        'icc': df_icc,
+        'bland_altman': df_bland,
+        'icc_sides': df_icc_sides,
+        'bland_altman_sides': df_bland_sides,
+    }
 
-        lims = [np.min([ax_2.get_xlim(), ax_2.get_ylim()]), np.max([ax_2.get_xlim(), ax_2.get_ylim()])]
+    for file_name, df in dict_dfs.items():
 
-        # Plot equality line
-        ax_2.plot(lims, lims, 'k-')
-
-        # Remove right and top borders
-        plt.gca().spines['right'].set_visible(False)
-        plt.gca().spines['top'].set_visible(False)
-
-        ax_2.tick_params(labelsize=25)
-
-        plt.xlabel("Zeno Walkway [cm]", fontsize=30)
-        plt.ylabel("Kinect [cm]", fontsize=30)
-        plt.tight_layout()
-
-        fig_2.savefig(join(dir_plots, 'compare_{}.png'.format(param)))
-
-    # %% Create tables of results
-
-    df_bland_alt = pd.DataFrame.from_records(list_bland_tuples, index=gait_params, columns=bland_alt._fields)
-
-    df_icc = pd.DataFrame.from_dict(dict_icc)
-
-    with open(join(dir_tables, 'bland_altman.txt'), 'w') as file:
-        file.write(np.round(df_bland_alt, 3).to_latex())
-
-    with open(join(dir_tables, 'icc.txt'), 'w') as file:
-        file.write(np.round(df_icc, 3).to_latex())
+        with open(join('results', 'tables', file_name + '.txt'), 'w') as file:
+            file.write(df.round(2).to_latex())
 
 
 if __name__ == '__main__':
