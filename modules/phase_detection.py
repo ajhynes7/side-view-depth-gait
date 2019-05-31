5de3969bad258b10d612c69dc2fe5949af48b4a4
Andrew Hynes <andrewhynes@ymail.com>
2019-05-31 16:29:37 -0400

    Change RANSAC to require inclusion of half the points
    This makes the results more consistent.

--- modules/phase_detection.py
+++ modules/phase_detection.py
@@ -17,8 +17,12 @@ import modules.numpy_funcs as nf
 def fit_ransac(points):
     """Fit a line to 3D points with RANSAC."""
 
-    thresh = 3 * min(mad(points))
-    model, is_inlier = ransac(points, LineModelND, min_samples=2, residual_threshold=thresh)
+    model, is_inlier = ransac(
+        points,
+        LineModelND,
+        min_samples=int(0.5 * len(points)),
+        residual_threshold=3 * min(mad(points)),
+    )
 
     return model, is_inlier
 
