ee242dc53439542e021d162c9c0a2387c0d5f144
Andrew Hynes <andrewhynes@ymail.com>
2019-06-05 12:38:27 -0400

    Add modules for dimension reduction and side assignment

---
+++ modules/dimension_reduction.py
@@ -0,0 +1,78 @@
+"""Module for converting 3D foot points to a 2D coordinate system."""
+
+from collections import namedtuple
+
+import numpy as np
+from dpcontracts import require, ensure
+from skimage.measure import LineModelND, ransac
+from skspatial.objects import Vector
+from statsmodels.robust import mad
+
+import modules.numpy_funcs as nf
+
+
+def fit_ransac(points):
+    """Fit a line to 3D points with RANSAC."""
+
+    model, is_inlier = ransac(
+        points, LineModelND, min_samples=int(0.5 * len(points)), residual_threshold=3 * min(mad(points))
+    )
+
+    return model, is_inlier
+
+
+def compute_basis(frames, points_head, points_a, points_b):
+    """Return origin and basis vectors of new coordinate system found with RANSAC."""
+
+    points_head_grouped = nf.interweave_rows(points_head, points_head)
+    points_foot_grouped = nf.interweave_rows(points_a, points_b)
+
+    frames_column = frames.reshape(-1, 1)
+    frames_grouped = nf.interweave_rows(frames_column, frames_column)
+
+    model_ransac, is_inlier = fit_ransac(points_foot_grouped)
+
+    points_head_inlier = points_head_grouped[is_inlier]
+    points_foot_inlier = points_foot_grouped[is_inlier]
+    frames_grouped_inlier = frames_grouped[is_inlier]
+
+    vector_up = Vector(np.median(points_head_inlier - points_foot_inlier, axis=0)).unit()
+    vector_forward = Vector(model_ransac.params[1]).unit()
+    vector_perp = vector_up.cross(vector_forward)
+
+    point_origin = model_ransac.params[0]
+
+    Basis = namedtuple('Basis', 'origin, forward, up, perp')
+    basis = Basis(point_origin, vector_forward, vector_up, vector_perp)
+
+    return basis, points_foot_inlier, frames_grouped_inlier
+
+
+@require("The input frames must be a 1D array.", lambda args: args.frames_grouped.ndim == 1)
+@ensure(
+    "The output frames must correspond to the points.""",
+    lambda _, result: result[0].size == result[1].shape[0],
+)
+@ensure("The output points must be all finite.", lambda _, result: np.isfinite(result[1]).all())
+def split_points(frames_grouped, points_grouped):
+    """Separate points corresponding to the same frame."""
+
+    frames_unique, counts_unique = np.unique(frames_grouped, return_counts=True)
+
+    n_objects = counts_unique.max()
+    is_max_count = counts_unique == n_objects
+
+    frames_final = frames_unique[is_max_count]
+    n_frames_final = frames_final.size
+
+    n_dim = points_grouped.shape[1]
+
+    points_stacked = np.full((n_frames_final, n_dim, n_objects), np.nan)
+
+    for i, frame in enumerate(frames_final):
+
+        is_frame = frames_grouped == frame
+
+        points_stacked[i] = points_grouped[is_frame].T
+
+    return frames_final, points_stacked
---
+++ modules/side_assignment.py
@@ -0,0 +1,64 @@
+"""Module for assigning left/right sides to the feet."""
+
+import numpy as np
+from dpcontracts import require, ensure
+from numpy.linalg import norm
+from scipy.signal import find_peaks
+from sklearn.preprocessing import normalize
+
+import modules.motion_correspondence as mc
+
+
+def split_walking_pass(frames, points_a, points_b):
+    """Split the walking pass between local minima in the foot distance signal."""
+
+    distances_foot = norm(points_a - points_b, axis=1)
+    distances_normalized = normalize(distances_foot.reshape(-1, 1), norm='max', axis=0).flatten()
+
+    indices_min, _ = find_peaks(1 - distances_normalized, prominence=0.5, width=1)
+
+    points_stacked = np.dstack((points_a, points_b))
+
+    # Split into portions between the detected peaks and remove portions that are too short.
+    # The portions should have at least three frames so motion correspondence algorithm can be run.
+    list_points = [x for x in np.split(points_stacked, indices_min) if len(x) >= 3]
+    list_frames = [x for x in np.split(frames, indices_min) if len(x) >= 3]
+
+    return list_frames, list_points
+
+
+@require("The points must be 2D.", lambda args: all(x.shape[1] == 2 for x in [args.points_a, args.points_b]))
+def assign_sides_pass(frames, points_a, points_b):
+    """Assign left/right sides to feet in a walking pass."""
+
+    list_frames, list_points = split_walking_pass(frames, points_a, points_b)
+
+    def yield_assigned_portions():
+        """Yield each portion of a walking pass with foot points assigned to left/right."""
+
+        for points_stacked_portion in list_points:
+
+            assignment = mc.correspond_motion(points_stacked_portion, [0, 1])
+            points_stacked_assigned = mc.assign_points(points_stacked_portion, assignment)
+
+            points_portion_l = points_stacked_assigned[:, :, 0]
+            points_portion_r = points_stacked_assigned[:, :, 1]
+
+            value_side_l = np.median(points_portion_l[:, 0])
+            value_side_r = np.median(points_portion_r[:, 0])
+
+            if value_side_l > value_side_r:
+
+                points_portion_l, points_portion_r = points_portion_r, points_portion_l
+
+            points_stacked_portion_lr = np.dstack((points_portion_l, points_portion_r))
+
+            yield points_stacked_portion_lr
+
+    points_stacked_lr = np.vstack([*yield_assigned_portions()])
+    frames_lr = np.concatenate(list_frames)
+
+    points_l = points_stacked_lr[:, :, 0]
+    points_r = points_stacked_lr[:, :, 1]
+
+    return frames_lr, points_l, points_r
