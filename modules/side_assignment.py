e4764cc9fe14e006e7914624b88169b3e021ce9f
Andrew Hynes <andrewhynes@ymail.com>
2019-06-05 13:29:54 -0400

    Edit side_assignment to use new numpy funcs for labelling sections
    This allows for easy filtering of sections that are too small for the motion correspondence algorithm.

--- modules/side_assignment.py
+++ modules/side_assignment.py
@@ -7,9 +7,10 @@ from scipy.signal import find_peaks
 from sklearn.preprocessing import normalize
 
 import modules.motion_correspondence as mc
+import modules.numpy_funcs as nf
 
 
-def split_walking_pass(frames, points_a, points_b):
+def split_walking_pass(points_a, points_b):
     """Split the walking pass between local minima in the foot distance signal."""
 
     distances_foot = norm(points_a - points_b, axis=1)
@@ -17,26 +18,34 @@ def split_walking_pass(frames, points_a, points_b):
 
     indices_min, _ = find_peaks(1 - distances_normalized, prominence=0.5, width=1)
 
-    points_stacked = np.dstack((points_a, points_b))
+    n_points = points_a.shape[0]
+    labels_sections = nf.label_by_split(indices_min, n_points)
 
-    # Split into sections between the detected peaks and remove sections that are too short.
-    # The sections should have at least three frames so motion correspondence algorithm can be run.
-    list_points = [x for x in np.split(points_stacked, indices_min) if len(x) >= 3]
-    list_frames = [x for x in np.split(frames, indices_min) if len(x) >= 3]
-
-    return list_frames, list_points
+    return labels_sections
 
 
 @require("The points must be 2D.", lambda args: all(x.shape[1] == 2 for x in [args.points_a, args.points_b]))
 def assign_sides_pass(frames, points_a, points_b):
     """Assign left/right sides to feet in a walking pass."""
 
-    list_frames, list_points = split_walking_pass(frames, points_a, points_b)
+    labels_sections = split_walking_pass(points_a, points_b)
+
+    # Mark small sections with label of -1.
+    labels_sections = nf.filter_labels(labels_sections, min_elements=3)
+    is_noise = labels_sections == -1
+
+    points_stacked = np.dstack((points_a, points_b))
 
     def yield_assigned_sections():
         """Yield each section of a walking pass with foot points assigned to left/right."""
 
-        for points_stacked_section in list_points:
+        labels_unique = np.unique(labels_sections[~is_noise])
+
+        for label in labels_unique:
+
+            is_section = labels_sections == label
+
+            points_stacked_section = points_stacked[is_section]
 
             assignment = mc.correspond_motion(points_stacked_section, [0, 1])
             points_stacked_assigned = mc.assign_points(points_stacked_section, assignment)
@@ -56,7 +65,7 @@ def assign_sides_pass(frames, points_a, points_b):
             yield points_stacked_section_lr
 
     points_stacked_lr = np.vstack([*yield_assigned_sections()])
-    frames_lr = np.concatenate(list_frames)
+    frames_lr = frames[~is_noise]
 
     points_l = points_stacked_lr[:, :, 0]
     points_r = points_stacked_lr[:, :, 1]
