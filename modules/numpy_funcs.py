46e64d2cb454e6e60f77adcd72a6a9c2be3ba4a5
Andrew Hynes <andrewhynes@ymail.com>
2019-06-05 13:28:44 -0400

    Delete calc_polynomial and add two new numpy funcs

--- modules/numpy_funcs.py
+++ modules/numpy_funcs.py
@@ -15,12 +15,91 @@ def interweave_rows(array_a, array_b):
     return array_c
 
 
-def calc_polynomial(xs, coeffs):
+def label_by_split(indices_split, n_elements):
+    """
+    Return an array of labels from split indices.
 
-    deg = len(coeffs) - 1
-    ys = np.zeros_like(xs)
+    The indices mark different sections of the array.
 
-    for i, coeff in enumerate(coeffs):
-        ys += coeff * xs ** (deg - i)
+    Parameters
+    ----------
+    indices_split : array_like
+        1D array of indices to split on.
+    n_elements: int
+        Desired length of the output array.
 
-    return ys
+    Returns
+    -------
+    ndarray
+        Array of labels.
+        Each label represents a section between the split indices.
+
+    Examples
+    --------
+    >>> label_by_split([1, 3], 4)
+    array([0, 1, 1, 2])
+
+    >>> label_by_split([2, 3, 5], 8)
+    array([0, 0, 1, 2, 2, 3, 3, 3])
+
+    >>> label_by_split([2, 3, 5], 10)
+    array([0, 0, 1, 2, 2, 3, 3, 3, 3, 3])
+
+    The number of elements can be smaller than the max split index.
+
+    >>> label_by_split([2, 3, 5], 4)
+    array([0, 0, 1, 2])
+
+    """
+    def yield_label_sections():
+
+        list_sections = np.split(np.zeros(n_elements), indices_split)
+
+        for i, array_section in enumerate(list_sections):
+            yield array_section + i
+
+    return np.concatenate([*yield_label_sections()]).astype(int)
+
+
+def filter_labels(labels, min_elements):
+    """
+    Return an array of labels with small groups marked as noise.
+
+    Parameters
+    ----------
+    labels : array_like
+        1D array of n labels.
+    min_elements : int
+        Minimum allowed number of labels in a group.
+
+    Returns
+    -------
+    labels_filtered : ndarray
+        (n,) array of labels. Labels belong to small groups changed to -1.
+
+    Examples
+    --------
+
+    >>> filter_labels([0, 0, 0, 1, 1, 2, 2, 2], 1)
+    array([0, 0, 0, 1, 1, 2, 2, 2])
+
+    >>> filter_labels([0, 0, 0, 1, 1, 2, 2, 2], 3)
+    array([ 0,  0,  0, -1, -1,  2,  2,  2])
+
+    The input labels do not need to be sorted.
+
+    >>> filter_labels([2, 0, 1, 0, 1, 0, 2, 2], 3)
+    array([ 2,  0, -1,  0, -1,  0,  2,  2])
+
+    """
+    labels = np.array(labels)
+    labels_filtered = np.array(labels)
+
+    for label in np.unique(labels):
+
+        is_label = labels == label
+
+        if is_label.sum() < min_elements:
+            labels_filtered[is_label] = -1
+
+    return labels_filtered
