a0c36215ed682fe9aee4126bfbc5ac050d43fbf2
Andrew Hynes <andrewhynes@ymail.com>
2019-06-27 10:57:34 -0400

    Change 'cols' coordinates from xyz to range(3)
    Now the cols are consistent in the codebase. Subtracting two DataArrays requires the same coordinates.

--- tests/unit/test_side_assignment.py
+++ tests/unit/test_side_assignment.py
@@ -39,7 +39,7 @@ def test_compute_basis():
 
     points_stacked = xr.DataArray(
         np.dstack((points_a, points_b, points_head)),
-        coords=(frames, ['x', 'y', 'z'], ['points_a', 'points_b', 'points_head']),
+        coords=(frames, range(3), ['points_a', 'points_b', 'points_head']),
         dims=('frames', 'cols', 'layers'),
     )
 
