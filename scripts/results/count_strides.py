f535053575a49ecdb79b70c785f917ced92c08e8
Andrew Hynes <andrewjhynes@gmail.com>
2020-02-18 22:22:13 -0500

    Add script to count strides (rows in the gait dataframe)

---
+++ scripts/results/count_strides.py
@@ -0,0 +1,27 @@
+"""Count strides measured by the Kinect."""
+
+from os.path import join
+
+import pandas as pd
+
+
+def main():
+
+    df_gait = pd.read_pickle(join('data', 'kinect', 'df_gait.pkl'))
+
+    list_stats = ['count', 'median']
+
+    stats_per_trial = df_gait.groupby('trial_name').size().agg(list_stats)
+    stats_per_pass = df_gait.groupby(['trial_name', 'num_pass']).size().agg(list_stats)
+
+    df_stats = (
+        pd.concat([stats_per_trial, stats_per_pass], axis=1)
+        .set_axis(["Per trial", "Per pass"], axis=1)
+    )
+
+    with open(join('results', 'tables', 'stride_counts.tsv'), 'w') as file:
+        file.write(df_stats.to_csv(sep='\t'))
+
+
+if __name__ == '__main__':
+    main()
