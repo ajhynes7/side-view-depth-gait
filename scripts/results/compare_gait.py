5048cc310ba281b2f05c9d7c5497755c45e0f801
Andrew Hynes <andrew@pop-os.localdomain>
2020-04-26 15:37:24 -0400

    Compute both relative and absolute Bland Altman
    Also join Bland and ICC data into one large table.

--- scripts/results/compare_gait.py
+++ scripts/results/compare_gait.py
@@ -10,7 +10,10 @@ from modules.typing import array_like
 
 
 def calc_trial_results(
-    df_matched_k: pd.DataFrame, df_matched_z: pd.DataFrame, vector_filter: Optional[array_like] = None
+    df_matched_k: pd.DataFrame,
+    df_matched_z: pd.DataFrame,
+    vector_filter: Optional[array_like] = None,
+    relative: bool = False,
 ) -> Tuple[pd.DataFrame, pd.DataFrame]:
     """Calculate ICC and Bland-Altman results."""
 
@@ -23,7 +26,7 @@ def calc_trial_results(
     assert vector_filter.ndim == 1
     assert df_trials_k.shape[0] == df_trials_z.shape[0] == vector_filter.size
 
-    dict_icc, dict_bland = {}, {}
+    dict_icc, dict_bland, dict_bland_relative = {}, {}, {}
 
     gait_params = df_matched_k.columns
 
@@ -32,21 +35,33 @@ def calc_trial_results(
         measures_k = df_trials_k[param][vector_filter]
         measures_z = df_trials_z[param][vector_filter]
 
-        measures = np.column_stack((measures_k, measures_z))
+        differences = measures_k - measures_z
+        differences_relative = st.relative_difference(measures_k, measures_z)
 
+        bland_altman = st.bland_altman(differences)
+        bland_altman_relative = st.bland_altman(differences_relative)
+
+        dict_bland[param] = bland_altman._asdict()
+        dict_bland_relative[param] = bland_altman_relative._asdict()
+
+        measures = np.column_stack((measures_k, measures_z))
         icc_21 = icc(measures, form=2)
         icc_31 = icc(measures, form=3)
 
-        differences = st.relative_difference(measures_k, measures_z)
-        bland_alt = st.bland_altman(differences)
-
         dict_icc[param] = {'ICC_21': icc_21, 'ICC_31': icc_31}
-        dict_bland[param] = bland_alt._asdict()
 
     df_icc = pd.DataFrame.from_dict(dict_icc, orient='index')
     df_bland = pd.DataFrame.from_dict(dict_bland, orient='index')
+    df_bland_relative = pd.DataFrame.from_dict(dict_bland_relative, orient='index')
 
-    return df_icc, df_bland
+    return df_bland, df_bland_relative, df_icc
+
+
+def concat_tables(df_bland, df_bland_relative, df_icc):
+
+    return pd.concat(
+        [df_bland, df_bland_relative * 100, df_icc], axis=1, keys=["Bland Altman", "Bland Altman Relative [%]", "ICC"]
+    ).round(2)
 
 
 def main():
@@ -63,25 +78,25 @@ def main():
     is_normal_walking = np.in1d(trials_kinect, trials_kinect_normal_walking)
 
     # Results from combining normal and dual-task walking
-    df_icc, df_bland = calc_trial_results(df_matched_k, df_matched_z)
+    df_bland, df_bland_relative, df_icc = calc_trial_results(df_matched_k, df_matched_z)
 
-    df_icc_normal, df_bland_normal = calc_trial_results(df_matched_k, df_matched_z, is_normal_walking)
-    df_icc_dual_task, df_bland_dual_task = calc_trial_results(df_matched_k, df_matched_z, ~is_normal_walking)
+    df_bland_normal, df_bland_rel_normal, df_icc_normal = calc_trial_results(
+        df_matched_k, df_matched_z, vector_filter=is_normal_walking
+    )
+    df_bland_dual_task, df_bland_rel_dual_task, df_icc_dual_task = calc_trial_results(
+        df_matched_k, df_matched_z, vector_filter=~is_normal_walking
+    )
 
-    df_icc_split = df_icc_normal.join(df_icc_dual_task, lsuffix='_normal', rsuffix='_dual_task')
-    df_bland_split = df_bland_normal.join(df_bland_dual_task, lsuffix='_normal', rsuffix='_dual_task')
+    df_concat = concat_tables(df_bland, df_bland_relative, df_icc)
+    df_concat_normal = concat_tables(df_bland_normal, df_bland_rel_normal, df_icc_normal)
+    df_concat_dual_task = concat_tables(df_bland_dual_task, df_bland_rel_dual_task, df_icc_dual_task)
 
-    dict_dfs = {
-        'icc': df_icc,
-        'bland_altman': df_bland,
-        'icc_split': df_icc_split,
-        'bland_altman_split': df_bland_split,
-    }
+    df_total = pd.concat(
+        [df_concat, df_concat_normal, df_concat_dual_task], keys=["Grouped", "Normal pace", "Dual task"]
+    )
 
-    for file_name, df in dict_dfs.items():
-
-        with open(join('results', 'tables', file_name + '.txt'), 'w') as file:
-            file.write(df.round(2).to_latex())
+    with open(join('results', 'tables', 'bland_icc.csv'), 'w') as file:
+        file.write(df_total.to_csv())
 
 
 if __name__ == '__main__':
